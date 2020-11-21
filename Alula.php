91cc6831b0a923b67fbe67d982cef5e47eff1960
nadia almutairi <nad7ia.almu@gmail.com>
2020-11-21 05:08:40 +0300

    Media

--- Alula.php
+++ Alula.php
@@ -2,7 +2,7 @@
   <html lang="ar" dir="rtl">
 <head>
 
-<title>Page Title</title>
+<title>العُلا</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="Style.css">
