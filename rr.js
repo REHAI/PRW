d44a3373dd0a7d254c3701d58c27dec0d26c43b5
raghad <74196961+raghadalaqili@users.noreply.github.com>
2020-11-22 21:43:40 +0300

    rr

---
+++ rr.js
@@ -0,0 +1,205 @@
+  <SCRIPT language=JavaScript>
+<!-- Begin
+/* Visit our site at http://www.elahmad.com/ for more code*/
+mydate = new Date();
+
+var year=mydate.getYear()
+if (year < 1000)
+year+=1900
+
+var myweekday=mydate.getDate()
+if (myweekday<10)
+myweekday="0"+myweekday
+
+myday = mydate.getDay();
+mymonth = mydate.getMonth();
+
+
+if(myday == 0)
+day = " Sunday"      
+else if(myday == 1)
+day = " Monday"
+else if(myday == 2)
+day = " Tuesday"   
+else if(myday == 3)
+day = " Wednesday"   
+else if(myday == 4)
+day = " Thursday"
+else if(myday == 5)
+day = " Friday"
+else if(myday == 6)
+day = "Saturday"
+if(mymonth == 0) {
+month = "1 "}
+else if(mymonth ==1)
+month = "2 "
+else if(mymonth ==2)
+month = "3 "
+else if(mymonth ==3)
+month = "4 "
+else if(mymonth ==4)
+month = "5 "
+else if(mymonth ==5)
+month = "6 "
+else if(mymonth ==6)
+month = "7 "
+else if(mymonth ==7)
+month = "8 "
+else if(mymonth ==8)
+month = "9 "
+else if(mymonth ==9)
+month = "10 "
+else if(mymonth ==10)
+month = "11 "
+else if(mymonth ==11)
+month = "12 "
+// End -->
+</SCRIPT>
+  
+    
+<CENTER>
+<SCRIPT>
+document.write("<b><font face=Tahoma color=#006600 size=2>" + day+ " - " +myweekday+ " - " +month + " - " + year + "</font></b>");
+</SCRIPT>
+</CENTER>
+
+
+<script type="text/javascript">
+
+/*Example message arrays for the two demo scrollers*/
+
+var pausecontent=new Array()
+pausecontent[0]='<img border="0" src="">'
+pausecontent[1]='<img border="0" src="bla.jpg">'
+pausecontent[2]='<img border="0" src="md.jpg">'
+pausecontent[3]='<img border="0" src="gr.jpg">'
+pausecontent[4]='<img border="0" src="backghroond.jpg">'
+
+pausecontent[5]='<img border="0" src="bla.jpg">'
+
+pausecontent[6]='<img border="0" src="athr.jpg">'
+
+pausecontent[7]='<img border="0" src="st.jpg">'
+pausecontent[8]='<img border="0" src="yane.jpeg">'
+pausecontent[9]='<img border="0" src="ll.jpg">'
+
+
+</script>
+
+<script type="text/javascript">
+
+/***********************************************
+* Pausing up-down scroller- � Dynamic Drive (www.dynamicdrive.com)
+***********************************************/
+
+function pausescroller(content, divId, divClass, delay){
+this.content=content //message array content
+this.tickerid=divId //ID of ticker div to display information
+this.delay=delay //Delay between msg change, in miliseconds.
+this.mouseoverBol=0 //Boolean to indicate whether mouse is currently over scroller (and pause it if it is)
+this.hiddendivpointer=1 //index of message array for hidden div
+document.write('<div id="'+divId+'" class="'+divClass+'" style="position: relative; overflow: hidden"><div class="innerDiv" style="position: absolute; width: 100%" id="'+divId+'1">'+content[0]+'</div><div class="innerDiv" style="position: absolute; width: 100%; visibility: hidden" id="'+divId+'2">'+content[1]+'</div></div>')
+var scrollerinstance=this
+if (window.addEventListener) //run onload in DOM2 browsers
+window.addEventListener("load", function(){scrollerinstance.initialize()}, false)
+else if (window.attachEvent) //run onload in IE5.5+
+window.attachEvent("onload", function(){scrollerinstance.initialize()})
+else if (document.getElementById) //if legacy DOM browsers, just start scroller after 0.5 sec
+setTimeout(function(){scrollerinstance.initialize()}, 500)
+}
+
+// -------------------------------------------------------------------
+// initialize()- Initialize scroller method.
+// -Get div objects, set initial positions, start up down animation
+// -------------------------------------------------------------------
+
+pausescroller.prototype.initialize=function(){
+this.tickerdiv=document.getElementById(this.tickerid)
+this.visiblediv=document.getElementById(this.tickerid+"1")
+this.hiddendiv=document.getElementById(this.tickerid+"2")
+this.visibledivtop=parseInt(pausescroller.getCSSpadding(this.tickerdiv))
+//set width of inner DIVs to outer DIV's width minus padding (padding assumed to be top padding x 2)
+this.visiblediv.style.width=this.hiddendiv.style.width=this.tickerdiv.offsetWidth-(this.visibledivtop*2)+"px"
+this.getinline(this.visiblediv, this.hiddendiv)
+this.hiddendiv.style.visibility="visible"
+var scrollerinstance=this
+document.getElementById(this.tickerid).onmouseover=function(){scrollerinstance.mouseoverBol=1}
+document.getElementById(this.tickerid).onmouseout=function(){scrollerinstance.mouseoverBol=0}
+if (window.attachEvent) //Clean up loose references in IE
+window.attachEvent("onunload", function(){scrollerinstance.tickerdiv.onmouseover=scrollerinstance.tickerdiv.onmouseout=null})
+setTimeout(function(){scrollerinstance.animateup()}, this.delay)
+}
+
+
+// -------------------------------------------------------------------
+// animateup()- Move the two inner divs of the scroller up and in sync
+// -------------------------------------------------------------------
+
+pausescroller.prototype.animateup=function(){
+var scrollerinstance=this
+if (parseInt(this.hiddendiv.style.top)>(this.visibledivtop+5)){
+this.visiblediv.style.top=parseInt(this.visiblediv.style.top)-5+"px"
+this.hiddendiv.style.top=parseInt(this.hiddendiv.style.top)-5+"px"
+setTimeout(function(){scrollerinstance.animateup()}, 50)
+}
+else{
+this.getinline(this.hiddendiv, this.visiblediv)
+this.swapdivs()
+setTimeout(function(){scrollerinstance.setmessage()}, this.delay)
+}
+}
+
+// -------------------------------------------------------------------
+// swapdivs()- Swap between which is the visible and which is the hidden div
+// -------------------------------------------------------------------
+
+pausescroller.prototype.swapdivs=function(){
+var tempcontainer=this.visiblediv
+this.visiblediv=this.hiddendiv
+this.hiddendiv=tempcontainer
+}
+
+pausescroller.prototype.getinline=function(div1, div2){
+div1.style.top=this.visibledivtop+"px"
+div2.style.top=Math.max(div1.parentNode.offsetHeight, div1.offsetHeight)+"px"
+}
+
+// -------------------------------------------------------------------
+// setmessage()- Populate the hidden div with the next message before it's visible
+// -------------------------------------------------------------------
+
+pausescroller.prototype.setmessage=function(){
+var scrollerinstance=this
+if (this.mouseoverBol==1) //if mouse is currently over scoller, do nothing (pause it)
+setTimeout(function(){scrollerinstance.setmessage()}, 100)
+else{
+var i=this.hiddendivpointer
+var ceiling=this.content.length
+this.hiddendivpointer=(i+1>ceiling-1)? 0 : i+1
+this.hiddendiv.innerHTML=this.content[this.hiddendivpointer]
+this.animateup()
+}
+}
+
+pausescroller.getCSSpadding=function(tickerobj){ //get CSS padding value, if any
+if (tickerobj.currentStyle)
+return tickerobj.currentStyle["paddingTop"]
+else if (window.getComputedStyle) //if DOM2
+return window.getComputedStyle(tickerobj, "").getPropertyValue("padding-top")
+else
+return 0
+}
+
+</script>
+<script type="text/javascript">
+
+//new pausescroller(name_of_message_array, CSS_ID, CSS_classname, pause_in_miliseconds)
+
+new pausescroller(pausecontent, "pscroller1", "someclass", 3000)
+document.write("<br />")
+
+</script>
+</center>
+
+<script language=javascript> window.alert('مرحبا بكم في اكتشاف العُلا '); </script>
+
