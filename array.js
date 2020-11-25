8edb483aa5e2616232169ff504b2d5f0e42c46d8
raghad <74196961+raghadalaqili@users.noreply.github.com>
2020-11-25 10:56:20 +0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,19 @@
+
+
+/*Example message arrays for the two demo scrollers*/
+
+var pausecontent=new Array()
+pausecontent[0]='<img border="0" src="">'
+pausecontent[1]='<img border="0" src="image/bla.jpg">'
+pausecontent[2]='<img border="0" src="image/md.jpg">'
+pausecontent[3]='<img border="0" src="image/gr.jpg">'
+pausecontent[4]='<img border="0" src="image/backghroond.jpg">'
+
+pausecontent[5]='<img border="0" src="image/bla.jpg">'
+
+pausecontent[6]='<img border="0" src="image/athr.jpg">'
+
+pausecontent[7]='<img border="0" src="image/st.jpg">'
+pausecontent[8]='<img border="0" src="image/yane.jpeg">'
+pausecontent[9]='<img border="0" src="image/ll.jpg">'
+
