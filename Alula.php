82dda3936c822cbbd51b2c1e2af911618dad6990
REHAI <reemhail423@gmail.com>
2020-11-27 20:51:01 +0300

    end

--- Alula.php
+++ Alula.php
@@ -21,91 +21,123 @@
           <li><a href="tourism.html">السياحة</a></li>
   </ul>
 </nav>
+
+
 <div class="image">
-  <img src="oll.jpg" alt="Alula" height="500" width="100%">
+  <img src="oll.jpg" alt="Alula" height="100%" width="100%">
 
   <div class="text">
-      <div class="p">   مـاهـي الـعُـلا ؟<br><br>
-                         شاهد الفيديو
+      <div class="p">
+         مـاهـي الـعُـلا ؟
+         <br><br>
+           شاهد الفيديو
         <a href="Videoalula.mp4">
           <img src="videoicon.png" alt="video"
-            height="80" width="80" > </a>
-          </div> </div>
+            height="80" width="80" >
+        </a>
+      </div>
+  </div>
 
 <br><br><br><br><br><br><br><br><br><br>
+
+<div class="clearboth"></div>
+
+
 
     <div class="img">
-      <img  src="bla.jpg" alt="بلدة العلا القديمة" height="500" width="750">
-    <div class="TitleoldAlula">
-      <h2> بلدة العلا القديمة</h2>
-                <p>    يمكن العثور في هذا الموقع الرائع على متاهةً
-                      من الشوارع المهجورة التي تضم 900 منزل من
-                     الطوب الطيني، و400 متجر، وخمس ساحات
-                     مع أزقةٍ متعرجةٍ ونوافذٍ تُطلّ على الممرات
-                     المخفية والتراسات على الأسطح التي تُلمِح
-                         فقط إلى العالم في الداخل. </p>  </div> </div>
+      <img  src="bla.jpg" alt="بلدة العلا القديمة">
+      <div class="TitleoldAlula">
+        <h2> بلدة العلا القديمة</h2>
+        <p>    يمكن العثور في هذا الموقع الرائع على متاهةً
+              من الشوارع المهجورة التي تضم 900 منزل من
+            الطوب الطيني، و400 متجر، وخمس ساحات
+            مع أزقةٍ متعرجةٍ ونوافذٍ تُطلّ على الممرات
+            المخفية والتراسات على الأسطح التي تُلمِح
+                فقط إلى العالم في الداخل.
+        </p>
+      </div>
+    </div>
 
-    <div class=" textimg">
-       <img src="old.jpg" alt="بلدة العلا القديمة" height="300" width="450" >
-          <p> بلدة العلا القديمة هي المكان الذي تجمع فيه الحجاج والمسافرون
-                            والمستوطنون الدائمون عبر العصور. </p></div>
+
+
+    <div class="img">
+       <img src="old.jpg" alt="بلدة العلا القديمة" >
+          <p class="p2"> بلدة العلا القديمة هي المكان الذي تجمع فيه الحجاج والمسافرون
+                            والمستوطنون الدائمون عبر العصور.
+          </p>
+    </div>
+
 
 <br><br><br><br><br><br><br><br><br><br>
 
-<div class="img ">
-  <img  src="akmah.jpg" alt="جبل عكمة" height="500" width="750">
+<div class="img">
+  <img  src="akmah.jpg" alt="جبل عكمة">
 <div class="TitleMountIkma">
   <h2> جبل عكمة</h2>
             <p>       ما كان في الماضي جزءً من محطة طريق
                القوافل، أصبح مع الزمن حارسًا يحفظ بدقة
              أحوال ذلك الزمن من خلال المنحوتات
                التي تُوفّر معلوماتٍ قيّمةٍ حول طريقة حياة
-               الدادانيين واللحيانيين وغيرهم </p>  </div>
+               الدادانيين واللحيانيين وغيرهم </p>
+  </div>
 
-<div class="textimg">
-   <img src="smikmah.jpg" alt="جبل عكمة" height="300" width="450" >
-      <p>   كان جبل عكمة موقعًا مهيبًا على طرق التجارة القديمة، ويضم أكثر من 500 كتابة
+
+<div class="img">
+   <img src="smikmah.jpg" alt="جبل عكمة" >
+      <p class="p2">   كان جبل عكمة موقعًا مهيبًا على طرق التجارة القديمة، ويضم أكثر من 500 كتابة
 منحوتة على المنحدرات والواجهات الصخرية التي تعود الى الفترتين الدادانية و اللحيانية.</p></div>
+
+
+
+
+
 
 <br><br><br><br><br><br><br><br><br><br>
 
-<div class=" img ">
-  <img  src="mo.jpg" alt="الديوان وجبل اثلب" height="500" width="750">
+<div class="img">
+  <img  src="mo.jpg" alt="الديوان وجبل اثلب">
 <div class="TitleDiwan">
   <h2> الديوان وجبل أثلب </h2>
             <p>       تمركزتْ الممارسات الدينية أو الطقسية في مدينة الحِجر حول جبل أثلب، وهو نتوءٌ جبليٌ طبيعيٌ يقع شرق المدينة.  </p>  </div> </div>
 
-<div class=" textimg">
-   <img src="moo.jpg" alt="الديوان وجبل اثلب" height="300" width="450" >
-  <p>"الديوان" وهي غرفة منحوتة في الصخور كانت ذات يوم مكانا ًلإقامة الولائم الفخمة وقاعة اجتماعات لقادة المدينة </p></div></div>
 
-<br><br><br><br><br><br><br><br><br><br>
+<div class="img">
+   <img src="moo.jpg" alt="الديوان وجبل اثلب">
+  <p class="p2">"الديوان" وهي غرفة منحوتة في الصخور كانت ذات يوم مكانا ًلإقامة الولائم الفخمة وقاعة اجتماعات لقادة المدينة </p></div></div>
+
+<br><br><br>
+
+<div class="clearboth"></div>
+
+<div class="div_down">
+
+  <div><img src="st.jpg" alt="استكشف العلا">
+  <p class="ExploreAlula">  اكتشف العلا</p></div>
 
 
-<div><img src="st.jpg" alt="استكشف العلا" height="400" width="500" style=" position: absolute; right:350px">
-<p class="ExploreAlula">  اكتشف العلا</p></div>
+  <div><img src="Sh.jpg" alt=" السياحة" >
+  <p class="tourism"> السياحة </p></div>
 
+  <div><img src="cxx.png" alt=" الحفلات">
+  <p class="Parties"> الحفلات والأنشطة </p></div>
 
-<div><img src="Sh.jpg" alt=" السياحة" height="400" width="500" style=" position:absolute;top:4300px;right:80px">
-<p  class="tourism"> السياحة </p></div>
+</div>
 
-<div><img src="cxx.png" alt=" الحفلات" height="200" width="500" style=" position: absolute; top:4300px; right:650px">
-  <img src="ll.jpg" alt=" الحفلات" height="200" width="500" style=" position: absolute; top:4500px;right:650px">
-<p class="Parties"> الحفلات والأنشطة </p></div>
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><b><br><br><br><br><br><br>
+
 
 
 <!-- Footer -->
 <div class="footer">
   <div class="llnker">
-    <img src="2030.png" alt="Vision 2020" width="100" height="100" style=" position:absolute; right:1100px">
     <h3 style="color:#FFCC00;font-weight:bold;font-size:25px;text-shadow: 2px 2px 2px #000000;">الروابط السريعة</h3>
     <a   href="https://youtu.be/KEVZ7K2Qrs0"> مناطيد العلا</a>
     <a   href="https://youtu.be/IgUB-PnE5C4"><br>حفل كارول سماحة</a>
-    <a   href="https://youtu.be/g3YbFn2ziqs"><br>الموسيقار عمر خيرت </a></div>
+    <a   href="https://youtu.be/g3YbFn2ziqs"><br>الموسيقار عمر خيرت </a>
+  </div>
 
 <div class="Subscribe">
     <h3 style="color:#FFCC00;font-weight:bold;font-size:25px;text-shadow: 2px 2px 2px  #000000;" > تواصل معنا</h3>
@@ -114,6 +146,14 @@
     <input  class="Subs"   type="email" name="uremail"  placeholder="البريد الإلكتروني" required>
     <input class="submit" type="submit" value="اشترك"><br>
 </div>
+
+
+<div class="logo_footer">
+  <img src="2030.png" alt="Vision 2020" width="100%">
+</div>
+
+
 </div>
 </body>
 </html>
+  <script language=javascript> window.alert('جولة للعلا '); </script>
