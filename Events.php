ee8a1dbe234ce485027b3ac8ecb50c99345192a0
REHAI <reemhail423@gmail.com>
2020-11-28 13:39:43 +0300

    r1

--- Events.php
+++ Events.php
@@ -26,55 +26,55 @@ https://www.w3schools.com/css/tryit.asp?filename=trycss_image_gallery -->
 <div class="Events">
    <a target="_link" href="https://youtu.be/IgUB-PnE5C4">
     <img src="karo.jpg" alt="karol" width="600" height="400"></a>
-  <div class="tex">حفل كارول سماحة في مهرجان شتاء طنطورة</div>
+  <div class="desc">حفل كارول سماحة في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
   <a target="_link" href="https://youtu.be/_hjCscXHzxQ">
     <img src="magedh.jpeg" alt="magedh" width="800" height="400"></a>
-  <div class="tex">حفل ماجده الرومي في مهرجان شتاء طنطورة</div>
+  <div class="desc">حفل ماجده الرومي في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
   <a target="_link" href="https://youtu.be/g3YbFn2ziqs">
     <img src="omar.jpeg" alt="omar" width="600" height="400"></a>
-  <div class="tex">حفل الفنان عمر خيرت في مهرجان شتاء طنطورة</div>
+  <div class="desc">حفل الفنان عمر خيرت في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
    <a target="_link" href="https://youtu.be/W7TzGapmvKQ">
     <img src="yane.jpeg" alt="yane" width="600" height="400"></a>
-  <div class="tex">حفل النجم ياني في مهرجان شتاء طنطورة</div>
+  <div class="desc">حفل النجم ياني في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
   <a target="_link" href="https://youtu.be/9iF922V2dUE">
     <img src="aziz.jpeg" alt="Forest" width="600" height="400"></a>
-  <div class="tex">حفل الفنانة عزيزة جلال في مهرجان شتاء طنطورة</div>
+  <div class="desc">حفل الفنانة عزيزة جلال في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
   <a target="_link" href="https://youtu.be/kNvoyGWxtus">
     <img src="beho.jpeg" alt="bethofn" width="600" height="400"></a>
-  <div class="tex">إحتفال بذكرى ميلاد بيتهوفن في مهرجان شتاء طنطورة</div>
+  <div class="desc">إحتفال بذكرى ميلاد بيتهوفن في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
 <a target="_link" href="https://youtu.be/7Z7xPhDMG1k">
 <img src="goz.jpeg" alt="gozeh" width="600" height="400"></a>
-<div class="tex">حفل جوزيه كاريراس في مهرجان شتاء طنطورة</div>
+<div class="desc">حفل جوزيه كاريراس في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
 <a target="_link" href="https://youtu.be/b6UlJcZFHWI">
 <img src="andr.jpeg" alt="andera" width="600" height="400"></a>
-<div class="tex">حفل أندريا بوتشيلي في مهرجان شتاء طنطورة</div>
+<div class="desc">حفل أندريا بوتشيلي في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
 <a target="_link" href="https://youtu.be/NbyjLCv-uP8">
 <img src="game.jpeg" alt="andera" width="600" height="400"></a>
-<div class="tex">حفل جاميروكواي في مهرجان شتاء طنطورة</div>
+<div class="desc">حفل جاميروكواي في مهرجان شتاء طنطورة</div>
 </div>
 
 <div class="Events">
@@ -86,13 +86,13 @@ https://www.w3schools.com/css/tryit.asp?filename=trycss_image_gallery -->
 <div class="Events">
 <a target="_link" href="https://youtu.be/jIY9uAmubZk">
 <img src="trath.jpeg" alt="andera" width="600" height="400"></a>
-<div class="tex"> أداء فرقة العلا للتراث الشعبي</div>
+<div class="desc"> أداء فرقة العلا للتراث الشعبي</div>
 </div>
 
 <div class="Events">
 <a target="_link" href="https://youtu.be/xNaQ1WWULXU">
 <img src="bolo.jpeg" alt="andera" width="600" height="400"></a>
-<div class="tex">بطولة العلا لبولو الصحراء في مهرجان شتاء طنطورة</div>
+<div class="desc">بطولة العلا لبولو الصحراء في مهرجان شتاء طنطورة</div>
 </div>
 
 </body>
