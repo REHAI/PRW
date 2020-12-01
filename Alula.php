9ab2628170e5ef1c9be53e99a65dd74ddd28406e
REHAI <reemhail423@gmail.com>
2020-12-01 10:28:51 +0300

    1

--- Alula.php
+++ Alula.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
   <html lang="ar" dir="rtl">
-<head>
 
+<head>
 <title>العلا</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="Style.css">
   <link rel = "icon" href ="2icon.jpg" type = "image/x-icon">
 </head>
+
 <body>
+  <script language=javascript> window.alert('جولة للعلا '); </script>
 <!-- Header -->
 <nav id="navbar" class="bg-dark">
   <div class="logo">
@@ -16,11 +18,13 @@
   </div>
   <ul>
     <li><a href="Alula.php">الرئيسية</a></li>
-      <li><a href="Home.php">أكتشف العلا</a></li>
-        <li><a href="events.php">الفعاليات والأنشطة </a></li>
-          <li><a href="tourism.php">السياحة</a></li>
+    <li><a href="Home.html">أكتشف العلا</a></li>
+      <li><a href="events.html">الفعاليات والأنشطة </a></li>
+        <li><a href="tourism.html">السياحة</a></li>
   </ul>
 </nav>
+
+<!-- Content -->
 <div class="image">
   <img src="oll.jpg" alt="Alula" height="500" width="100%">
 
@@ -29,8 +33,7 @@
                          شاهد الفيديو
         <a href="Videoalula.mp4">
           <img src="videoicon.png" alt="video"
-            height="80" width="80" > </a>
-          </div> </div>
+            height="80" width="80" > </a>  </div> </div>
 
 <br><br><br><br><br><br><br><br><br><br>
 
@@ -43,12 +46,12 @@
                      الطوب الطيني، و400 متجر، وخمس ساحات
                      مع أزقةٍ متعرجةٍ ونوافذٍ تُطلّ على الممرات
                      المخفية والتراسات على الأسطح التي تُلمِح
-                         فقط إلى العالم في الداخل. </p>  </div> </div>
+                         فقط إلى العالم في الداخل.   </p> </div> </div>
 
     <div class=" textimg">
        <img src="old.jpg" alt="بلدة العلا القديمة" height="300" width="450" >
           <p> بلدة العلا القديمة هي المكان الذي تجمع فيه الحجاج والمسافرون
-                            والمستوطنون الدائمون عبر العصور. </p></div>
+                            والمستوطنون الدائمون عبر العصور. </p>  </div>
 
 <br><br><br><br><br><br><br><br><br><br>
 
@@ -60,7 +63,7 @@
                القوافل، أصبح مع الزمن حارسًا يحفظ بدقة
              أحوال ذلك الزمن من خلال المنحوتات
                التي تُوفّر معلوماتٍ قيّمةٍ حول طريقة حياة
-               الدادانيين واللحيانيين وغيرهم </p>  </div>
+               الدادانيين واللحيانيين وغيرهم </p> </div>
 
 <div class="textimg">
    <img src="smikmah.jpg" alt="جبل عكمة" height="300" width="450" >
@@ -73,7 +76,7 @@
   <img  src="mo.jpg" alt="الديوان وجبل اثلب" height="500" width="750">
 <div class="TitleDiwan">
   <h2> الديوان وجبل أثلب </h2>
-            <p>       تمركزتْ الممارسات الدينية أو الطقسية في مدينة الحِجر حول جبل أثلب، وهو نتوءٌ جبليٌ طبيعيٌ يقع شرق المدينة.  </p>  </div> </div>
+            <p>  تمركزتْ الممارسات الدينية أو الطقسية في مدينة الحِجر حول جبل أثلب، وهو نتوءٌ جبليٌ طبيعيٌ يقع شرق المدينة.  </p>  </div> </div>
 
 <div class=" textimg">
    <img src="moo.jpg" alt="الديوان وجبل اثلب" height="300" width="450" >
@@ -83,15 +86,15 @@
 
 
 <div><img src="st.jpg" alt="استكشف العلا" height="400" width="500" style=" position: absolute; right:350px">
-<p class="ExploreAlula">  اكتشف العلا</p></div>
+<a class="ExploreAlula" href="Home.php">أكتشف العلا</a></div>
 
 
 <div><img src="Sh.jpg" alt=" السياحة" height="400" width="500" style=" position:absolute;top:4300px;right:80px">
-<p  class="tourism"> السياحة </p></div>
+ <a  class="tourism" href="tourism.php">السياحة</a></div>
 
 <div><img src="cxx.png" alt=" الحفلات" height="200" width="500" style=" position: absolute; top:4300px; right:650px">
   <img src="ll.jpg" alt=" الحفلات" height="200" width="500" style=" position: absolute; top:4500px;right:650px">
-<p class="Parties"> الحفلات والأنشطة </p></div>
+<a class="Parties" href="events.php">الفعاليات والأنشطة </a> </div>
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
@@ -105,16 +108,18 @@
     <h3 style="color:#FFCC00;font-weight:bold;font-size:25px;text-shadow: 2px 2px 2px #000000;">الروابط السريعة</h3>
     <a   href="https://youtu.be/KEVZ7K2Qrs0"> مناطيد العلا</a>
     <a   href="https://youtu.be/IgUB-PnE5C4"><br>حفل كارول سماحة</a>
-    <a   href="https://youtu.be/g3YbFn2ziqs"><br>الموسيقار عمر خيرت </a></div>
+    <a   href="https://youtu.be/g3YbFn2ziqs"><br>الموسيقار عمر خيرت </a>
+  <a href="Vis.php"  ><br>عدد الاشتراكات </a>
+  </div>
 
-<div class="Subscribe">
-    <h3 style="color:#FFCC00;font-weight:bold;font-size:25px;text-shadow: 2px 2px 2px  #000000;" > تواصل معنا</h3>
-    <h5>اشترك للحصول على أحدث أخبار العلا من العروض والفعاليات</h5>
-    <form action="final.php" method="post">
-    <input  class="Subs"   type="email" name="uremail"  placeholder="البريد الإلكتروني" required>
-    <input class="submit" type="submit" value="اشترك"><br>
-</div>
-</div>
+
+    <div class="Subscribe">
+        <h3 style="color:#FFCC00;font-weight:bold;font-size:25px;text-shadow: 2px 2px 2px  #000000;" > تواصل معنا</h3>
+        <h5>اشترك للحصول على أحدث أخبار العلا من العروض والفعاليات</h5>
+        <form action="final.php" method="post">
+        <input  class="Subs"   type="email" name="uremail"  placeholder="البريد الألكتروني" required>
+        <input class="submit" type="submit" value="اشترك"><br>
+    </div>
+    </div>
 </body>
 </html>
-  <script language=javascript> window.alert('جولة للعلا '); </script>
